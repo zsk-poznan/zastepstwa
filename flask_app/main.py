09ac99bb7ef9349046ad87a8559697c00e724901
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-12-15 12:05:05 +0100

    Move to separate folder

--- flask_app/main.py
+++ flask_app/main.py
@@ -3,26 +3,33 @@ import get_data
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/zastepstwa')
+
+@app.route("/zastepstwa")
 def zastepstwa():
     teachers = get_data.main()
     return render_template("z.html", teachers=teachers)
 
-@app.route('/zastepstwa-pelne')
+
+@app.route("/zastepstwa-pelne")
 def zastepstwa_original():
     teachers = get_data.main()
     return render_template("z-pelne.html", teachers=teachers, i=0, len=len)
 
-@app.route('/nauczyciel', methods=['GET'])
+
+@app.route("/nauczyciel", methods=["GET"])
 def nauczyciel():
     teachers = get_data.main()
     teacher = request.args.get("name")
-    return render_template("nauczyciel.html", teachers=teachers, teacher=teacher, len=len, i=0)
+    return render_template(
+        "nauczyciel.html", teachers=teachers, teacher=teacher, len=len, i=0
+    )
 
-@app.route('/komunikaty')
+
+@app.route("/komunikaty")
 def komunikaty():
     return render_template("komunikaty.html")
