fd4eb7339498443eb1aac82de58f405aa700869c
AdamSiekierski <adam.siekiera@outlook.com>
2019-12-21 12:12:13 +0100

    Finish the teacher detail view

--- frontend/src/view/Teacher.jsx
+++ frontend/src/view/Teacher.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import TeacherTitle from '../components/TeacherTitle';
+import TeacherTable from '../components/TeacherTable';
 
 const Teacher = () => {
   const { name } = useParams();
+  const [substitutions, setSubstitutions] = useState([]);
+
+  // Here the substituions for the teacher will be downloaded from API
+  // Simulated async call
+  setTimeout(() => {
+    const newSubstitutions = [
+      {
+        lekcja: '1',
+        zamiast: 'Jan Kowalski',
+        oddział: '3E',
+        przedmiot: 'Pracownia Urządzeń Techniki Komputerowej',
+        sala: 'SG1',
+        uwagi: 'Brak',
+      },
+      {
+        lekcja: '5',
+        zamiast: 'John Doe',
+        oddział: '1C',
+        przedmiot: 'Sieci Komputerowe',
+        sala: '44',
+        uwagi: 'Brak',
+      },
+    ];
+    setSubstitutions(newSubstitutions);
+  }, 1000);
 
   return (
     <div
@@ -14,6 +40,7 @@ const Teacher = () => {
       }}
     >
       <TeacherTitle title={name} />
+      <TeacherTable substitutions={substitutions} />
     </div>
   );
 };
