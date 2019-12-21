3fdd3c83cf6fae442ceacddbdd5354f64d395350
Kacper Latuszewski <kacpx123@gmail.com>
2019-12-21 18:18:06 +0100

    delete versioning

--- flask_app/main.py
+++ flask_app/main.py
@@ -1,15 +1,15 @@
-from flask import Flask, escape, request, render_template, jsonify, make_response
+from flask import Flask, jsonify, make_response
 import get_data
 
 app = Flask(__name__)
 
 
-@app.route("/api/v0.1/", methods=["GET"])
+@app.route("/api/", methods=["GET"])
 def get_all():
     teachers = get_data.main()
     return jsonify({'data': teachers})
 
-@app.route("/api/v0.1/<string:name>", methods=["GET"])
+@app.route("/api/<string:name>", methods=["GET"])
 def get_by_teacher(name):
     teachers = get_data.main()
     teacher = teachers.get(name)
