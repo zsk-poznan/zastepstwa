3185750f3623fb0897f346f4210d4fcea086b13c
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-01-08 19:45:56 +0100

    Add simple view

---
+++ frontend/src/view/AllSubstitutions.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+import StyledTable from '../components/StyledTable';
+
+const url = 'https://localhost:5000';
+
+const AllSubstitutions = () => {
+  const [substitutions, setSubstitutions] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${url}/api/teacher/all`)
+      .then(({ data }) => setSubstitutions(data.data));
+  }, []);
+
+  return (
+    <div>
+      <StyledTable>
+        <thead>
+          <tr>
+            <th>Lekcja</th>
+            <th>Zamiast</th>
+            <th>Oddział</th>
+            <th>Przedmiot</th>
+            <th>Sala</th>
+            <th>Uwagi</th>
+            <th>Nauczyciel</th>
+          </tr>
+        </thead>
+        <tbody>
+          {substitutions.map((substitution) => (
+            <tr key={substitution.lesson_id}>
+              <td>{substitution.lesson_id}</td>
+              <td>{substitution.teacher}</td>
+              <td>{substitution.group}</td>
+              <td>{substitution.lesson_name}</td>
+              <td>{substitution.classroom}</td>
+              <td>{substitution.notes || 'Brak'}</td>
+              <td>{substitution.substitute_teacher}</td>
+            </tr>
+          ))}
+        </tbody>
+      </StyledTable>
+    </div>
+  );
+};
+
+export default AllSubstitutions;
