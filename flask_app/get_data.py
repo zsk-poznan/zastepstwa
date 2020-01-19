b03bbab31681931e06f0a5a88d4dd5b07daa4271
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2020-01-19 15:30:26 +0100

    Make linter happy

--- flask_app/get_data.py
+++ flask_app/get_data.py
@@ -4,16 +4,16 @@ import json
 import codecs
 from bs4 import BeautifulSoup
 
+
 def main():
-    with codecs.open("Zastępstwa.html", "r", "utf-8") as f: # Zastępstwa.html in main folder
-        soup = BeautifulSoup(f.read(), 'html.parser')
+    with codecs.open(
+        "Zastępstwa.html", "r", "utf-8"
+    ) as f:  # Zastępstwa.html in main folder
+        soup = BeautifulSoup(f.read(), "html.parser")
 
-    rows = soup.find_all('tr')
+    rows = soup.find_all("tr")
 
-    data = [
-        row.find_all('td')
-        for row in rows
-    ]
+    data = [row.find_all("td") for row in rows]
 
     del data[:2]  # deletes useless two first lines
 
