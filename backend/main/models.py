259dc310992e4729cb24e20ddd0c19dae59d83a0
Kacper Latuszewski <kacpx123@gmail.com>
2020-02-28 21:36:02 +0100

    created substitiution model

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class SubstitutionModel(models.Model):
   lesson_number = models.IntegerField()
   absent_teacher = models.CharField(max_length=30)
--- backend/subsitutions/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class SubsitutionsConfig(AppConfig):
-    name = 'subsitutions'
